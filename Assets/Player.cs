27220a18235e0f6a2a63e0aa8624f94bfe4600e8
Vinay <vxm200033@utdallas.edu>
2024-04-16 14:59:14 -0500

    More Scripts

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Stats:")]
+    [SerializeField]
+    private int _attack = 5;
+    [SerializeField]
+    private int _playerHealth = 100;
+
+}
