931db9f501662654e48e97e7aeb7a5add79e5243
Vinay <vxm200033@utdallas.edu>
2024-04-16 10:37:27 -0500

    Monster Start Script

---
+++ Assets/Monster1.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster1 : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField]
+    private string _name;
+}
