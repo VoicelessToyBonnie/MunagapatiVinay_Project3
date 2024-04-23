1b3dd095117388456f3b8e284034f0266ec34f83
Vinay <vxm200033@utdallas.edu>
2024-04-23 14:52:42 -0500

    LevelUp Script

--- Assets/XP/LevelUp.cs
+++ Assets/XP/LevelUp.cs
@@ -1,8 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelUp : MonoBehaviour
 {
+    [Header("Experience")]
+    [SerializeField] AnimationCurve experienceCurve;
 
+    int currentLevel, totalExperience;
+    int previousLevelsExperience, nextLevelsExperience;
+
+    [Header("Interface")]
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] Image experienceFill;
+
+    void Start()
+    {
+        UpdateLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AddExperience(5);
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        totalExperience += amount;
+        CheckForLevelUp();
+        UpdateInterface();
+    }
+
+    void CheckForLevelUp()
+    {
+        if(totalExperience >= nextLevelsExperience)
+        {
+            currentLevel++;
+            UpdateLevel();
+        }
+    }
+
+    void UpdateLevel()
+    {
+        previousLevelsExperience = (int)experienceCurve.Evaluate(currentLevel);
+        nextLevelsExperience = (int)experienceCurve.Evaluate(currentLevel + 1);
+        UpdateInterface();
+    }
+
+    void UpdateInterface()
+    {
+        int start = totalExperience - previousLevelsExperience;
+        int end = nextLevelsExperience - previousLevelsExperience;
+
+        levelText.text = currentLevel.ToString();
+        experienceFill.fillAmount = (float)start / (float)end;
+    }
 }
